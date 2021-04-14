9cb858c4b106a82cb7ce882bb9dbc9de1328577e
sahabuddin <sahabuddinriyaj984@gmail.com>
2021-04-14 16:52:46 +0600

    aboutus courses facts section complete

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:false,
+    navText: ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
